df705d5489c9b2e0eafd4c2c81216241bbe7548b
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-02-20 16:56:35 +0300

    Update index1.js

--- Lab 7 - Client JS/Country Explorer/js/index1.js
+++ Lab 7 - Client JS/Country Explorer/js/index1.js
@@ -1,7 +1,21 @@
-const regionsBaseUrl = "https://restcountries.com/v3.1/region/"
+const regionsBaseUrl = "https://restcountries.com/v3.1/region"
 const countryBaseUrl = "https://restcountries.com/v3.1/name"
 const regionSearchBar = document.querySelector('#region')
+const countryDropDown = document.querySelector('#country')
 
-function getCountries() {
-    alert(regionSearchBar.value)
+async function getCountries() {
+    const url = `${regionsBaseUrl}/${regionSearchBar.value}`
+    const data = await fetch(url)
+    const countries = await data.json()
+    const options = countries.map(country => convertToOption(country))
+    countryDropDown.innerHTML = options.join(' ')
+}
+
+function loadCountryDetails() {
+  //receive which country is selected
+    //
+}
+
+function convertToOption(country) {
+    return `<option value="${country.name.common}"> ${country.name.common} </option>`
 }
