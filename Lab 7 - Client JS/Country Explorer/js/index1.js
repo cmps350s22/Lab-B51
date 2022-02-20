35744915e9c56ddfdc0642507c09a984a8238763
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-02-20 16:27:18 +0300

    updated

---
+++ Lab 7 - Client JS/Country Explorer/js/index1.js
@@ -0,0 +1,7 @@
+const regionsBaseUrl = "https://restcountries.com/v3.1/region/"
+const countryBaseUrl = "https://restcountries.com/v3.1/name"
+const regionSearchBar = document.querySelector('#region')
+
+function getCountries() {
+    alert(regionSearchBar.value)
+}
