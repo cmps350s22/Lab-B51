78820f1ee8cd6a9143f9f5ee38d6255f9303cef0
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-02-27 17:43:07 +0300

    Update index.js

--- Lab 8 - Client Data Management/Tutorial/CensusApp/js/index.js
+++ Lab 8 - Client Data Management/Tutorial/CensusApp/js/index.js
@@ -1,37 +1,66 @@
 import {CensusRepo} from './repository/census-repo.js'
 const censusRepo = new CensusRepo()
 
+window.onload = async ()=>{
+    await showCensusList()
+    window.deleteCensus = deleteCensus
+}
+
 // const db = new Localbase('census.db')
 const addBtn = document.querySelector('#add-btn')
 const formElement = document.querySelector('#form')
 const censusTable = document.querySelector('#countries')
+const noOfRowsDD = document.querySelector('#noOfRows')
 
 
 formElement.addEventListener('submit', addCensus)
+noOfRowsDD.addEventListener('change', showCensusList)
 //registering your methods
 
-showCensusList()
+
+
+//1. create a function that converts one census object into a row
+//2. map all the census into HTML rows by calling that function
+//3. Inject
+
+async function deleteCensus(id){
+    await censusRepo.deleteCensus(id)
+    await showCensusList()
+}
 
 async function showCensusList() {
-    const allCensus = await censusRepo.getAllCensus()
+    const noOfRows = parseInt(noOfRowsDD.value)
+    const allCensus = await censusRepo.getAllCensus(noOfRows)
     console.log(allCensus)
-    
+    const censusHTMLRows = allCensus.map(census => censusHTMLRow(census)).join(' ')
     censusTable.innerHTML = `
         <tr> 
             <th>Country</th>
             <th>Population</th>
             <th>Action</th>
         </tr>
-    
+        ${censusHTMLRows}
     `
 }
-
+function censusHTMLRow(census) {
+    return `
+        <tr>
+            <td>${census.country}</td>
+            <td>${census.population}</td>
+            <td>
+                <i class="fa fa-edit"> Edit </i>
+                <i class="fa fa-trash" onclick="deleteCensus(${census.id})"> Delete </i>
+            </td>
+        </tr>
+    `
+}
 async function addCensus(e) {
     e.preventDefault()
-    console.log(e.target)
     const census = formToObject(e.target)
     census.id = Date.now()
     const result =  await censusRepo.addCensus(census);
+    await showCensusList()
+    formElement.reset()
 }
 
 
