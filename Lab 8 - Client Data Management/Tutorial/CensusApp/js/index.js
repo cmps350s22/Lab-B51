4cc7585a1c8be87edd6dcdf7bad21b4b14677771
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-02-27 17:10:49 +0300

    Update index.js

--- Lab 8 - Client Data Management/Tutorial/CensusApp/js/index.js
+++ Lab 8 - Client Data Management/Tutorial/CensusApp/js/index.js
@@ -1,19 +1,45 @@
-// import {CensusRepo} from './repository/census-repo.js'
+import {CensusRepo} from './repository/census-repo.js'
+const censusRepo = new CensusRepo()
 
 // const db = new Localbase('census.db')
 const addBtn = document.querySelector('#add-btn')
 const formElement = document.querySelector('#form')
+const censusTable = document.querySelector('#countries')
 
 
 formElement.addEventListener('submit', addCensus)
 //registering your methods
 
-function addCensus(e) {
+showCensusList()
+
+async function showCensusList() {
+    const allCensus = await censusRepo.getAllCensus()
+    console.log(allCensus)
+    
+    censusTable.innerHTML = `
+        <tr> 
+            <th>Country</th>
+            <th>Population</th>
+            <th>Action</th>
+        </tr>
+    
+    `
+}
+
+async function addCensus(e) {
     e.preventDefault()
     console.log(e.target)
+    const census = formToObject(e.target)
+    census.id = Date.now()
+    const result =  await censusRepo.addCensus(census);
 }
 
 
 function formToObject(form) {
     const formData = new FormData(form)
+    const data = {}
+    for (const [key, value] of formData) {
+        data[key] = value
+    }
+    return data;
 }
