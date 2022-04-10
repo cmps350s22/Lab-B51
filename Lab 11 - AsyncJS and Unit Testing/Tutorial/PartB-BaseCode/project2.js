dd3adab815ab76366937b66747b91d9df18001d6
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-04-10 21:17:24 +0300

    Update project2.js

--- Lab 11 - AsyncJS and Unit Testing/Tutorial/PartB-BaseCode/project2.js
+++ Lab 11 - AsyncJS and Unit Testing/Tutorial/PartB-BaseCode/project2.js
@@ -9,4 +9,43 @@ function getCourses(cb)
 4)	Instructor names are set as a new property to the course object in the setInstrctorNames function.
 
  */
+import fs from 'fs';
 
+function getCourses(cb) {
+    fs.readFile('data/course.json', (err, data) => {
+        if (!err) {
+            const courses = JSON.parse(data)
+            fs.readFile('data/staff.json', (err, data) => {
+                if (!err) {
+                    const staffs = JSON.parse(data)
+                    for (const course of courses) {
+                        const {
+                            firstname: firstname,
+                            lastname
+                        } = staffs.find(staff => staff.staffNo == course.instructorId)
+                        course.instructor = `${firstname} ${lastname}`
+                        // delete course.instructorId
+                    }
+                    fs.readFile('data/staff.json', (err, data) => {
+                        if (!err) {
+
+                        } else {
+                            cb(err, null)
+                        }
+                    })
+                } else {
+                    cb(err, null)
+                }
+            })
+        } else
+            cb(err, null)
+    })
+}
+
+
+getCourses((err, courses) => {
+    if (!err)
+        console.log(courses)
+    else
+        console.log(err)
+})
