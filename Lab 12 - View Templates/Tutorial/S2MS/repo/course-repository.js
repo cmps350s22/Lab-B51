1ff341ee52137deaa9e79daa3ec831bf6c1f8769
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-04-17 20:08:58 +0300

    lab 12

---
+++ Lab 12 - View Templates/Tutorial/S2MS/repo/course-repository.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+export default class CourseRepository{
+    constructor() {
+    }
+    async getPrograms(){
+        const data = await fetch('https://cmps356s17.github.io/data/ceng-programs.json');
+        const programs = await data.json();
+        return programs;
+    }
+    async getCourses(programCode){
+        let data = await fetch('https://cmps356s17.github.io/data/ceng-courses.json');
+        let courses = await data.json();
+        return courses.filter(c=>c.program === programCode.toUpperCase());
+    }
+}
+
+
